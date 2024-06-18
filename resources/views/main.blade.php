21f682d66669781489554bad0f1e3c4a02b0049e
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2024-06-17 21:23:15 -0400

    añadido etiqueda de acceso inseguro

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="Start your development with FoodHut landing page.">
-        <meta name="author" content="Devcrud">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>Pasteleria Pepita</title>
 
         <!-- font icons -->
