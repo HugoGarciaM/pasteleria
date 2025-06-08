15b51be154e324c8f4254d186819d7402ac1c034
andy <andy@localhost.localdomain>
2025-06-08 01:39:46 -0400

    Agregacion de boton delivery y asignacion del mismo
    revisen

--- resources/views/personal/saleProcess.blade.php
+++ resources/views/personal/saleProcess.blade.php
@@ -22,6 +22,18 @@
                     <th>Cliente</th>
                 </thead>
                 <tbody>
+                    <tr>
+                        <td>
+                            <a class="btn btn-warning" onclick="openDeliveryModal(null)">
+                                    <i class="fa fa-motorcycle"></i> Delivery
+                                </a>
+
+
+                        </td>
+
+
+                    </tr>
+    
                     @foreach ($sales as $sale)
                         <tr>
                             <td>{{ $sale->id }}</td>
@@ -37,6 +49,9 @@
                                     <i class="fa fa-trash"></i>
                                     Cancelar
                                 </a>
+                                <a class="btn btn-warning" onclick="openDeliveryModal({{ $sale->id }})">
+                                    <i class="fa fa-motorcycle"></i> Delivery
+                                </a>
                             </td>
                         </tr>
                     @endforeach
@@ -50,6 +65,28 @@
             <a class="btn btn-danger mt-1" onclick="closeTransaction()">Cancelar</a>
         </div>
     </x-modal>
+    <x-modal id="mDelivery" title="Delivery a Domicilio">
+    <div class="row justify-content-center p-3">
+        <div class="col-12 text-center mb-2">
+            <h5 style="font-weight: bold; color: #4e73df; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
+                Conductores disponibles:
+            </h5>
+        </div>
+
+        <div class="col-8">
+            <select class="form-select text-center">
+                <option disabled selected>Seleccione un delivery</option>
+                <option>Delevery1</option>
+                <option>Delivery2</option>
+            </select>
+        </div>
+
+        <div class="col-12 text-center mt-3">
+            <a class="btn btn-success me-2" onclick="assignDelivery()">Asignar</a>
+            <a class="btn btn-secondary" onclick="closeDeliveryModal()">Cerrar</a>
+        </div>
+    </div>
+</x-modal>
 @endsection
 
 @section('js')
@@ -98,5 +135,15 @@
             clearInterval(interval);
             $('#mTransaction').modal('hide');
         }
+        function openDeliveryModal(id) {
+            const modal = new bootstrap.Modal(document.getElementById('mDelivery'));
+            modal.show();
+        }
+
+        function closeDeliveryModal() {
+            $('#mDelivery').modal('hide');
+        }
+        
     </script>
+    
 @endsection
